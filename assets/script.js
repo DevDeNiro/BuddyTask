64c0547b2f95839cad33813b2a39c6131fc5f8c9
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2022-04-24 16:27:34 +0200

    ...

--- assets/script.js
+++ assets/script.js
@@ -7,21 +7,25 @@
 
 var hint = document.getElementById("raw");
 var btn = document.getElementById("check");
+var content = document.getElementById("flex-content");
 
 btn.addEventListener("click", function () {
   hint.className = hint.className !== "show" ? "show" : "hide";
   if (hint.className === "hide") {
     hint.style.display = "block";
+    content.style.display = "block";
     window.setTimeout(function () {
       hint.style.opacity = 1;
       hint.style.transform = "translateX(0)";
+      content.style.transform = "translateX(0)";
     }, 0);
   }
   if (hint.className === "show") {
     hint.style.opacity = 0;
     hint.style.transform = "translateX(-20px)";
+    content.style.transform = "translateX(-20px)";
     window.setTimeout(function () {
       hint.style.display = "none";
-    }, 500); // timed to match animation-duration
+    }, 200); // timed to match animation-duration
   }
 });
