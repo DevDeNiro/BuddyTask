47b90e8fbfaeb8b1b767e379d7ee35929a72a9c7
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2023-05-01 21:54:21 +0200

    Can drag it :alien:

--- todo-list-app/src/components/tasks/TaskList.vue
+++ todo-list-app/src/components/tasks/TaskList.vue
@@ -1,74 +1,86 @@
 <template>
-  <draggable v-model="localTasks" :group="{name: 'tasks'}" @end="onDragEnd">
-    <div
-      v-for="(task, index) in filterTasks.incompleteTasks"
-      :key="index"
-      class="card cursor-pointer border border-gray-300 rounded-lg p-4 my-4"
-    >
-      <div class="flex">
-        <div class="bg-blue-600 w-1"></div>
+  <Draggable v-model="localTasks" :group="{name: 'tasks'}" @end="onDragEnd">
+    <template #item="{element}">
+      <div>
+        <div
+          v-for="(task, index) in filterTasks.incompleteTasks"
+          :key="index"
+          class="card cursor-pointer border border-gray-300 rounded-lg p-4 my-4"
+        >
+          <div class="flex">
+            <div class="bg-blue-600 w-1"></div>
 
-        <div class="flex items-center">
-          <div class="flex items-center">
-            <div class="h-7 w-1 bg-blue-600 mr-2"></div>
-            <span class="material-icons text-xl px-2 pb-4">check</span>
+            <div class="flex items-center">
+              <div class="flex items-center">
+                <div class="h-7 w-1 bg-blue-600 mr-2"></div>
+                <span class="material-icons text-xl px-2 pb-4">check</span>
+              </div>
+              <div class="flex flex-col ml-2">
+                <div>
+                  <h3 class="font-bold">{{ task.name }}</h3>
+                  <p class="text-sm">{{ task.description }}</p>
+                </div>
+                <span class="text-sm mt-4">
+                  <span>
+                    {{ formatDate(task.startDate) }} -
+                    {{ formatDate(task.endDate) }}
+                  </span>
+                </span>
+              </div>
+            </div>
           </div>
-          <div class="flex flex-col ml-2">
-            <h3 class="font-bold">{{ task.name }}</h3>
-            <p class="text-sm">{{ task.description }}</p>
-            <span class="text-sm mt-4">
-              {{ formatDate(task.startDate) }} -
-              {{ formatDate(task.endDate) }}
-            </span>
+        </div>
+
+        <div class="relative flex py-5 items-center decoration-dashed">
+          <div class="flex-grow border-t border-gray-400"></div>
+          <span class="flex-shrink mx-4 text-gray-400 uppercase font-semibold"
+            >done</span
+          >
+          <div class="flex-grow border-t border-gray-400"></div>
+        </div>
+
+        <div
+          v-for="(task, index) in filterTasks.completeTasks"
+          :key="index"
+          class="card cursor-pointer border border-gray-300 rounded-lg p-4 my-4"
+        >
+          <div class="flex">
+            <div class="bg-blue-600 w-1"></div>
+
+            <div class="flex items-center">
+              <div class="flex items-center">
+                <div class="h-7 w-1 bg-blue-600 mr-2"></div>
+                <span class="material-icons text-xl px-2 pb-4">check</span>
+              </div>
+              <div class="flex flex-col ml-2">
+                <div>
+                  <h3 class="font-bold">{{ task.name }}</h3>
+                  <p class="text-sm">{{ task.description }}</p>
+                </div>
+                <span class="text-sm mt-4">
+                  <span>
+                    {{ formatDate(task.startDate) }} -
+                    {{ formatDate(task.endDate) }}
+                  </span>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <div class="relative flex py-5 items-center decoration-dashed">
-      <div class="flex-grow border-t border-gray-400"></div>
-      <span class="flex-shrink mx-4 text-gray-400 uppercase font-semibold"
-        >done</span
-      >
-      <div class="flex-grow border-t border-gray-400"></div>
-    </div>
-
-    <div
-      v-for="(task, index) in filterTasks.completeTasks"
-      :key="index"
-      class="card cursor-pointer border border-gray-300 rounded-lg p-4 my-4"
-    >
-      <div class="flex">
-        <div class="bg-blue-600 w-1"></div>
-
-        <div class="flex items-center">
-          <div class="flex items-center">
-            <div class="h-7 w-1 bg-blue-600 mr-2"></div>
-            <span class="material-icons text-xl px-2 pb-4">check</span>
-          </div>
-          <div class="flex flex-col ml-2">
-            <h3 class="font-bold">{{ task.name }}</h3>
-            <p class="text-sm">{{ task.description }}</p>
-            <span class="text-sm mt-4">
-              {{ formatDate(task.startDate) }} -
-              {{ formatDate(task.endDate) }}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </draggable>
+    </template>
+  </Draggable>
 </template>
 
 <script>
 import {ref, computed} from "vue";
 import {defineComponent} from "vue";
-import draggable from "vuedraggable";
+import Draggable from "vuedraggable";
 
 export default defineComponent({
   name: "TaskList",
   components: {
-    draggable,
+    Draggable,
   },
   props: {
     tasks: {
