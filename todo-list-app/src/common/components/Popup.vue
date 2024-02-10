9afe747656b0a5b411db49777ed47a0ce45cb3eb
DevDeNiro <72260596+DevDeNiro@users.noreply.github.com>
2024-02-10 10:29:32 +0100

    update form-style

--- todo-list-app/src/common/components/Popup.vue
+++ todo-list-app/src/common/components/Popup.vue
@@ -4,16 +4,15 @@
         v-if="popup.isVisible"
         class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm"
     >
-      <div class="bg-white rounded shadow-lg p-6 w-full max-w-sm">
-        <p class="text-gray-800" :class="typeClass">{{ message }}</p>
+      <div class="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm">
+        <div class="flex justify-between items-start pb-3">
+          <p class="text-gray-800" :class="typeClass">{{ message }}</p>
+          <button @click="closePopup">
+            X
+          </button>
+        </div>
         <!--Dynamic field for props-->
         <slot></slot>
-        <button
-            @click="closePopup"
-            class="mt-4 bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
-        >
-          Close
-        </button>
       </div>
     </div>
   </transition>
@@ -31,18 +30,6 @@ export default {
     const store = useStore();
     const popup = computed(() => store.getters.popup);
     const message = computed(() => popup.value.message);
-    const type = computed(() => popup.value.type);
-
-    const typeClass = (type) => {
-      console.log('type', type);
-      const typeClasses = {
-        info: "text-blue-500",
-        warning: "text-yellow-500",
-        error: "text-red-500",
-        success: "text-green-500",
-      };
-      return typeClasses[type] || "text-gray-500";
-    };
 
     const closePopup = () => {
       store.dispatch("hidePopup");
@@ -51,8 +38,6 @@ export default {
     return {
       popup,
       message,
-      type,
-      typeClass,
       closePopup,
     };
   },
--- todo-list-app/src/features/tasks/TaskForm.vue
+++ todo-list-app/src/features/tasks/TaskForm.vue
@@ -1,25 +1,72 @@
 <template>
-  <form @submit.prevent="submitForm">
+  <form @submit.prevent="submitForm" CLASS="flex flex-col">
     <Popup>
-      <div v-for="(field, index) in fields" :key="index">
-        <label :for="field.name" class="block">{{ field.label }}</label>
+      <!-- Titre -->
+      <div class="mx-auto">
+        <label :for="fields[0].name" class="block">{{ fields[0].label }}</label>
         <input
-          :id="field.name"
-          :name="field.name"
-          :type="field.type"
-          :placeholder="field.placeholder"
-          class="border rounded p-1 w-full"
-          v-model="formData[field.name]"
-          required
+            :id="fields[0].name"
+            :name="fields[0].name"
+            :type="fields[0].type"
+            :placeholder="fields[0].placeholder"
+            class="border rounded p-1 w-full my-1"
+            v-model="formData[fields[0].name]"
+            required
         />
       </div>
-      <button @click.prevent="submitForm">Create</button>
+
+      <!-- Dates group -->
+      <div class="flex justify-between max-[768]:flex-column my-1">
+        <div>
+          <label :for="fields[1].name" class="block mb-1">{{ fields[1].label }}</label>
+          <input
+              :id="fields[1].name"
+              :name="fields[1].name"
+              :type="fields[1].type"
+              :placeholder="fields[1].placeholder"
+              class="border rounded  w-full "
+              v-model="formData[fields[1].name]"
+              required
+          />
+        </div>
+        <div>
+          <label :for="fields[2].name" class="block mb-1">{{ fields[2].label }}</label>
+          <input
+              :id="fields[2].name"
+              :name="fields[2].name"
+              :type="fields[2].type"
+              :placeholder="fields[2].placeholder"
+              class="border rounded w-full"
+              v-model="formData[fields[2].name]"
+              required
+          />
+        </div>
+      </div>
+
+      <!-- Description -->
+      <div class="mx-auto my-2">
+        <label :for="fields[3].name" class="block mb-1">{{ fields[3].label }}</label>
+        <textarea
+            :id="fields[3].name"
+            :name="fields[3].name"
+            :placeholder="fields[3].placeholder"
+            class="border rounded p-1 w-full h-24 my-1"
+            v-model="formData[fields[3].name]"
+            required
+        ></textarea>
+      </div>
+
+      <button type="submit"
+              class="font-bold"
+              @click.prevent="submitForm">Create
+      </button>
     </Popup>
   </form>
 </template>
 
+
 <script>
-import {ref, computed} from "vue";
+import {computed, ref} from "vue";
 import Popup from "../../common/components/Popup.vue";
 import {useStore} from "vuex";
 
@@ -45,28 +92,24 @@ export default {
         label: "Titre",
         type: "text",
         placeholder: "Entrez un titre",
-        class: "border rounded p-1 w-full",
       },
       {
         name: "startDate",
         label: "Date de début",
         type: "date",
         placeholder: "Sélectionnez une date",
-        class: "border rounded p-1 w-full",
       },
       {
         name: "endDate",
         label: "Date de fin",
         type: "date",
         placeholder: "Sélectionnez une date",
-        class: "border rounded p-1 w-full",
       },
       {
         name: "description",
         label: "Description",
         type: "textarea",
         placeholder: "Entrez une description",
-        class: "border rounded p-1 w-full",
       },
     ]);
 
@@ -77,11 +120,12 @@ export default {
       formData.value.categoryId = props.category.id;
 
       store
-        .dispatch("addTask", formData.value)
-        .then(() => {})
-        .finally(() => {
-          store.dispatch("hidePopup");
-        });
+          .dispatch("addTask", formData.value)
+          .then(() => {
+          })
+          .finally(() => {
+            store.dispatch("hidePopup");
+          });
     };
 
     return {
